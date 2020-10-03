51f7c3f2a0941d9990e0b66730e426df76587260
Aniket <57316188+aniket328@users.noreply.github.com>
2020-10-03 13:06:49 +0530

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,52 +1,17 @@
 #include <iostream>
 
 using namespace std;
-class T2;
-class T1
-{
-    int a;
-public:
-    T1(){}
-    void geta(int x)
-    {
-        a=x;
-    }
-    void display()
-    {
-        cout<< " a = " << a<< endl;
-    }
-    friend void operator>(T1,T2);
 
-};
-
-class T2
+template<class ani>
+ani add(ani a, ani b)
 {
-    int b;
-public:
-    void getb(int x)
-    {
-        b=x;
-    }
-    void display()
-    {
-        cout<< " b = " << b<< endl;
-    }
-    friend void operator>(T1,T2);
-};
-
-void operator>(T1 m, T2 n)
-{
-    m.a>n.b?cout<<"a is bigger"<<endl:cout<<"b is bigger"<<endl;
+    return a+b;
 }
+
 int main()
 {
-    T1 x;
-    T2 y;
-    x.geta(17);
-    y.getb(9);
-    x.display();
-    y.display();
-    x>y;
-    cout << "Hello world!" << endl;
+    double x=32.678, y=78, z;
+    z=add(x,y);
+    cout<< "The sum is: "<< z << endl;
     return 0;
 }
